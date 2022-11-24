7e661c94347bed014b4621d9fd98bc38c8cfee25
Egor S. Orlov <Egor.Orlov@avalon.ru>
2022-11-24 23:15:37 +0300

    dynamic uid

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const http = require('http');
+const os = require("os");
+const userInfo = os.userInfo();
+const uid = userInfo.uid;
 
 const hostname = '0.0.0.0';
-const port = 500;
+const port = 1000 + uid;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Hello UID=', uid);
 });
 
 server.listen(port, hostname, () => {
